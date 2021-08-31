cc820159ec094b3a9eccbd392b21c8da8a0ed8e8
Erhuvwu <ayisirebless@gmail.com>
2021-08-31 08:40:34 -0500

    my tardis

---
+++ tardis/src/SecondComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SecondComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h3></h3>
+            </div>
+        );
+    }
+}
+
+export default SecondComponent;
---
+++ tardis/src/ThirdComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ThirdComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h3></h3>tardis
+            </div>
+        );
+    }
+}
+
+export default ThirdComponent;
