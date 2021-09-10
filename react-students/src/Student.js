6fb3d519ec1ab4d749f9f2544ef09d779dc692bd
Erhuvwu <ayisirebless@gmail.com>
2021-09-10 12:48:09 -0500

    redo

--- react-students/src/Student.js
+++ react-students/src/Student.js
@@ -4,7 +4,8 @@ function Student(props) {
     console.log(props)
     return (
         <div>
-           <h2>Name: {props.names}</h2>
+           <h2>Name: {props.name}</h2>
+           <h2>Bio: {props.bio}</h2>
         </div>
     );
 }
